1af792ab049275354822a9462b100883fe79a666
JUNYOUNG <plus4070@gmail.com>
2015-12-19 21:42:25 +0900

    change root-context.xml

--- Basecamp_PreHomework/src/main/java/com/basecamp/prehomework/BoardController.java
+++ Basecamp_PreHomework/src/main/java/com/basecamp/prehomework/BoardController.java
@@ -2,10 +2,14 @@ package com.basecamp.prehomework;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Locale;
 
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,30 +20,37 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class BoardController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
-	
+
+	@Autowired
+	private SqlSession sqlSession;
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
-		
+
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
+
 		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
+
+		model.addAttribute("serverTime", formattedDate);
+		HashMap<String, String> input = new HashMap<String, String>();
+		input.put("bContent", "content");
+		List<HashMap<String, String>> outputs = sqlSession.selectList("userControlMapper.selectSample", input);
+		System.out.println(outputs.toString());
 		
 		return "home";
 	}
-	
-	@RequestMapping(value="/index", method = RequestMethod.GET)
-	public String index(Model model){
-		
+
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(Model model) {
+
 		return "index";
 	}
-	
+
 }
